43148f83071ec7a863d5e45ef09b550205506457
Derek Kim <ddkim1024@gmail.com>
2020-10-07 18:00:14 -0500

    Updates

---
+++ precache-manifest.c18e717f4b7e650158e09904fadfc133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1677197a9f085f11770d222edffd709",
+    "url": "/memory-card-game/index.html"
+  },
+  {
+    "revision": "c12dc0a1bc782a60e8f8",
+    "url": "/memory-card-game/static/css/main.9096163c.chunk.css"
+  },
+  {
+    "revision": "483e4a6899fa43ea51ad",
+    "url": "/memory-card-game/static/js/2.e4a68b2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-card-game/static/js/2.e4a68b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12dc0a1bc782a60e8f8",
+    "url": "/memory-card-game/static/js/main.08abdecb.chunk.js"
+  },
+  {
+    "revision": "9da7231189e0e08fd771",
+    "url": "/memory-card-game/static/js/runtime-main.799ae9f3.js"
+  }
+]);
